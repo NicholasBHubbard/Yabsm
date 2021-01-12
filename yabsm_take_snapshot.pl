b2d7bf1b8f3dff574ddc555e087546874acd3b79
Nicholas Hubbard <nhub73@keemail.me>
2021-01-12 14:08:21 -0500

    made significant progress on yabsm_update_conf.pl

--- yabsm_take_snapshot.pl
+++ yabsm_take_snapshot.pl
@@ -23,147 +23,147 @@ use 5.010;
 
 use Getopt::Long;
 
-               ####################################
-               #     PROCESS INPUT PARAMETERS     #
-               ####################################
+                 ####################################
+                 #     PROCESS INPUT PARAMETERS     #
+                 ####################################
 
-my $SUBVOL_TO_SNAPSHOT_ARG;
+my $SUBVOL_MOUNTPOINT_ARG;
 my $SNAPSHOT_ROOT_DIR_ARG;
-my $YABSM_SUBVOL_ARG;
+my $YABSM_SUBVOL_NAME_ARG;
 my $TIMEFRAME_ARG;
 my $SNAPS_TO_KEEP_ARG;
 
-GetOptions ('subv=s'      => \$SUBVOL_TO_SNAPSHOT_ARG,
+GetOptions ('mntpoint=s'  => \$SUBVOL_MOUNTPOINT_ARG,
             'snapdir=s'   => \$SNAPSHOT_ROOT_DIR_ARG,
-            'subvname=s'  => \$YABSM_SUBVOL_ARG,
+            'subvname=s'  => \$YABSM_SUBVOL_NAME_ARG,
             'timeframe=s' => \$TIMEFRAME_ARG,
             'keeping=i'   => \$SNAPS_TO_KEEP_ARG);
 
-foreach my $arg ($SUBVOL_TO_SNAPSHOT_ARG, 
+foreach my $arg ($SUBVOL_MOUNTPOINT_ARG,
                  $SNAPSHOT_ROOT_DIR_ARG,
-                 $YABSM_SUBVOL_ARG,
+                 $YABSM_SUBVOL_NAME_ARG,
                  $TIMEFRAME_ARG,
                  $SNAPS_TO_KEEP_ARG) {
-  die "a required command line argument was not provided"
-    if !defined $arg;
+    die 'required: {--mntpoint, --snapdir, --subvname, --timeframe, --keeping}'
+      if !defined $arg;
 }
 
-my $WORKING_DIR = "${SNAPSHOT_ROOT_DIR_ARG}/${YABSM_SUBVOL_ARG}/$TIMEFRAME_ARG";
+my $WORKING_DIR =
+  "${SNAPSHOT_ROOT_DIR_ARG}/${YABSM_SUBVOL_NAME_ARG}/$TIMEFRAME_ARG";
 
-               ####################################
-               #               MAIN               #
-               ####################################
+                 ####################################
+                 #               MAIN               #
+                 ####################################
 
 delete_appropriate_snapshots(); 
 take_new_snapshot();
 
-               ####################################
-               #          SNAPSHOT CREATION       #
-               ####################################
+                 ####################################
+                 #          SNAPSHOT CREATION       #
+                 ####################################
 
 sub take_new_snapshot {
-
-  system('btrfs subvolume snapshot '
-         . $SUBVOL_TO_SNAPSHOT_ARG . ' '
-         . $WORKING_DIR . '/'
-         . create_snapshot_name()) == 0 # system() returns exit status.
-           or die "unable to create new snapshot in \"${WORKING_DIR}\"";
-  return;
+    
+    system('btrfs subvolume snapshot '
+           . $SUBVOL_MOUNTPOINT_ARG . ' '
+           . $WORKING_DIR . '/'
+           . create_snapshot_name()) == 0 # system() returns exit status
+             or die "unable to create new snapshot in \"${WORKING_DIR}\"";
+    return;
 }
 
 sub create_snapshot_name { 
-
-  my ($min, $hr, $day, $mon, $yr) =
-    map { sprintf '%02d', $_ }(localtime)[1..5];
-
-  $mon++;      # month count starts at zero. 
-  $yr += 1900; # year represents years since 1900. 
-
-  return "day=${yr}_${mon}_${day},time=${hr}:$min";
+    
+    my ($min, $hr, $day, $mon, $yr) =
+      map { sprintf '%02d', $_ }(localtime)[1..5];
+    
+    $mon++;      # month count starts at zero. 
+    $yr += 1900; # year represents years since 1900. 
+    
+    return "day=${yr}_${mon}_${day},time=${hr}:$min";
 }
 
-               ####################################
-               #        SNAPSHOT DELETION         #
-               ####################################
+                 ####################################
+                 #        SNAPSHOT DELETION         #
+                 ####################################
 
 sub delete_appropriate_snapshots {
-
-  opendir(my $dh, $WORKING_DIR)
-    or die "failed to open directory $WORKING_DIR: $!";
-
-  my @existing_snaps = grep(/^[^\.]/, readdir $dh); # exclude dot files
-
-  closedir $dh;
-
-  my $num_snaps = scalar @existing_snaps;
-
-  if ($num_snaps < $SNAPS_TO_KEEP_ARG) { # don't delete any snapshots.
-    return; 
-  } 
-  elsif ($num_snaps > $SNAPS_TO_KEEP_ARG) { # user changed settings to keep 
-                                               # less snapshots.
-    for (my $i = 0; $i <= $num_snaps - $SNAPS_TO_KEEP_ARG; $i++) {
-
-      my $snap_to_delete = earliest_snap(\@existing_snaps);
-
-      delete_snapshot $snap_to_delete;
-
-      @existing_snaps = grep $_ ne $snap_to_delete, @existing_snaps;
+    
+    opendir(my $dh, $WORKING_DIR)
+      or die "failed to open directory $WORKING_DIR: $!";
+    
+    my @existing_snaps = grep(/^[^\.]/, readdir $dh); # exclude dot files
+    
+    closedir $dh;
+    
+    my $num_snaps = scalar @existing_snaps;
+    
+    if ($num_snaps < $SNAPS_TO_KEEP_ARG) { # don't delete any snapshots
+        return; 
+    } 
+    elsif ($num_snaps > $SNAPS_TO_KEEP_ARG) { # user changed prefs to keep less
+        
+        for (my $i = 0; $i <= $num_snaps - $SNAPS_TO_KEEP_ARG; $i++) {
+            
+            my $snap_to_delete = earliest_snap(\@existing_snaps);
+            
+            delete_snapshot($snap_to_delete);
+            
+            @existing_snaps = grep $_ ne $snap_to_delete, @existing_snaps;
+        }
+    } 
+    else { # delete just one snapshot.
+        delete_snapshot(earliest_snap(\@existing_snaps)); 
     }
-  } 
-  else { # delete just one snapshot.
-    delete_snapshot(earliest_snap(\@existing_snaps)); 
-  }
-  return;
+    return;
 }
 
 sub delete_snapshot {
-
-  system('btrfs subvolume delete '
-         . $WORKING_DIR . '/'
-         . $_[0]) == 0
-           or die "failed to delete subvolume \"${WORKING_DIR}/$_[0]\"";
-  return;
+    
+    system('btrfs subvolume delete '
+           . $WORKING_DIR . '/'
+           . $_[0]) == 0 # system() returns exit status
+             or die "failed to delete subvolume \"${WORKING_DIR}/$_[0]\"";
+    return;
 }
 
 sub earliest_snap {
-
-  my $earliest_snap = @{$_[0]}[0]; 
-
-  foreach my $snap (@{$_[0]}) {
-    $earliest_snap = $snap if snapshot_lt($snap,$earliest_snap);
-  }
-  return $earliest_snap;
+    
+    my $earliest_snap = @{$_[0]}[0]; 
+    
+    foreach my $snap (@{$_[0]}) {
+        $earliest_snap = $_ if snapshot_lt($snap,$earliest_snap);
+    }
+    return $earliest_snap;
 }
 
 sub snapshot_lt { 
-
-  return lexically_lt(snap_name_to_lex_ord_nums($_[0]),
-                      snap_name_to_lex_ord_nums($_[1]));
+    return lexically_lt(snap_name_to_lex_ord_nums($_[0]),
+                        snap_name_to_lex_ord_nums($_[1]));
 }
 
 sub lexically_lt {
-
-  my ($head1, @tail1) = @{$_[0]};
-  my ($head2, @tail2) = @{$_[1]};
-
-  if ($head1 > $head2) {
-    return 0;
-  }
-  elsif ($head1 < $head2) {
-    return 1;
-  }
-  elsif (@tail1 == 0 && @tail2 == 0) { # array args must always be equal length.
-    return 0;
-  }
-  else {
-    return lexically_lt(\@tail1,\@tail2);
-  }
+    
+    my ($head1, @tail1) = @{$_[0]};
+    my ($head2, @tail2) = @{$_[1]};
+    
+    if ($head1 > $head2) {
+        return 0;
+    }
+    elsif ($head1 < $head2) {
+        return 1;
+    }
+    elsif (@tail1 == 0 && @tail2 == 0) { # array args must be equal length
+        return 0;
+    }
+    else {
+        return lexically_lt(\@tail1,\@tail2);
+    }
 }
 
 sub snap_name_to_lex_ord_nums {
-
-  my ($yr, $mon, $day, $hr, $min) = $_[0] =~ m/([0-9]+)/g;
-
-  return [$yr,$mon,$day,$hr,$min];
+    
+    my ($yr, $mon, $day, $hr, $min) = $_[0] =~ m/([0-9]+)/g;
+    
+    return [$yr,$mon,$day,$hr,$min];
 }
--- yabsm_update_conf.pl
+++ yabsm_update_conf.pl
@@ -8,14 +8,168 @@ use strict;
 use warnings;
 use 5.010;
 
-               ####################################
-               #         GRAB USER SETTINGS       #
-               ####################################
+use Data::Dumper;
 
-open (my $fh, '<:encoding(UTF-8)', '/etc/yabsmrc')
-  or die 'failed to open file /etc/yabsmrc: $!';
+                 ####################################
+                 #               MAIN               #
+                 ####################################
 
-my @lines = grep { m/^[a-z]/ } readline $fh;
+my %YABSMRC_HASH = yabsmrc_to_hash();
 
-close $fh;
+check_valid_config();
+write_cronjobs();
 
+                 ####################################
+                 #         PARSE CONFIG FILE        #
+                 ####################################
+
+sub yabsmrc_to_hash {
+    
+    open my $fh, '<:encoding(UTF-8)', '/etc/yabsmrc'
+      or die 'failed to open file \"/etc/yabsmrc\": $!';
+    
+    my %yabsmrc_hash;
+    
+    foreach my $line (<$fh>) {
+        
+        next if ($line =~ /^[^a-zA-Z]/);
+        
+        my ($key, $val) = split /=/, $line;
+        
+        chomp $val; # $val is appended with newline
+        
+        if ($key eq 'I_want_to_snap_this_subvol') { 
+            push @{$yabsmrc_hash{$key}}, $val; # note this is an array
+        }
+        else {
+            $yabsmrc_hash{$key} = $val;
+        }
+    }
+    
+    close $fh;
+    return %yabsmrc_hash;
+}
+
+                 ####################################
+                 #      CHECK CONFIG FOR ERRORS     #
+                 ####################################
+
+sub check_valid_config {
+    
+    my @subvols_to_check = @{$YABSMRC_HASH{'I_want_to_snap_this_subvol'}};
+    
+    foreach (@subvols_to_check) {
+        
+        my ($subv, undef) = split /,/, $_;
+        
+        my @settings = my ($hourly_take, $hourly_keep,
+                           $daily_take, $daily_keep,
+                           $midnight_want, $midnight_keep,
+                           $monthly_want, $monthly_keep) 
+          = grab_settings_for($subv);
+        
+        die ("\"$subv\" is missing one of these required settings:\n"
+             . "${subv}_hourly_take   | ${subv}_hourly_keep\n${subv}_daily_take"
+             . "    | ${subv}_daily_keep\n${subv}_midnight_want | "
+             . "${subv}_midnight_keep \n${subv}_monthly_want "
+             . " | ${subv}_monthly_keep\n $!")
+          if grep { ! defined } (@settings);
+        
+        die ("max value for \"${subv}_hourly_take\" is '60'")
+          if ($hourly_take > 60);
+        
+        die ("max value for \"${subv}_daily_take\" is '24'")
+          if ($daily_take > 24);
+        
+        die ("value for \"${subv}_midnight_want\" must be \"yes\" or \"no\"")
+          if ! ($midnight_want ne 'yes' xor $midnight_want ne 'no');
+        
+        die ("value for \"${subv}_monthly_want\" must be \"yes\" or \"no\"")
+          if ! ($monthly_want ne 'yes' xor $monthly_want ne 'no');
+    }
+    return;
+}
+
+                 ####################################
+                 #           WRITE CRONJOBS         #
+                 ####################################
+
+sub write_cronjobs {
+    
+    my @cronjobs;
+    
+    my @subvols_to_snap = @{$YABSMRC_HASH{'I_want_to_snap_this_subvol'}};
+    
+    foreach (@subvols_to_snap) {
+        
+        my ($subv_name, $mountpoint) = split /,/, $_;
+        
+        my @cron_strings = create_cronjob_strings_for($subv_name,$mountpoint);
+    }
+    return;
+}
+
+sub create_cronjob_strings_for {
+    
+    my $subv_name = shift;
+    my $mountpoint = shift;
+    
+    my $snapshot_dir = $YABSMRC_HASH{'snapshot_directory'}; 
+    
+    my ($hourly_take, $hourly_keep,
+        $daily_take, $daily_keep,
+        $midnight_want, $midnight_keep,
+        $monthly_want, $monthly_keep) = grab_settings_for($subv_name); 
+    
+    my $hourly_cron = ('*/' . int(60 / $hourly_take)
+                       . ' * * * * root /root/yabsm/yabsm_take_snapshot '
+                       . "--mntpoint $mountpoint --snapdir $snapshot_dir "
+                       . "--subvname $subv_name --timeframe hourly "
+                       . "--keeping $hourly_keep");
+    
+    my $daily_cron = ('0 */' . int(24 / $daily_take)
+                      . ' * * * root /root/yabsm/yabsm_take_snapshot '
+                      . "--mntpoint $mountpoint --snapdir $snapshot_dir "
+                      . "--subvname $subv_name --timeframe daily "
+                      . "--keeping $daily_keep");
+    
+    my $midnight_cron = ('0 0 * * * root /root/yabsm/yabsm_take_snapshot '
+                         . "--mntpoint $mountpoint --snapdir $snapshot_dir "
+                         . "--subvname $subv_name --timeframe midnight "
+                         . "--keeping $midnight_keep")
+      unless !defined $midnight_want;
+    
+    my $monthly_cron = ('0 0 1 * * root /rootyabsm/yabsm_take_snapshot '
+                        . "--mntpoint $mountpoint --snapdir $snapshot_dir "
+                        . "--subvname $subv_name --timeframe monthly "
+                        . "--keeping $monthly_keep")
+      unless !defined $monthly_want;
+    
+    return [$hourly_cron, $daily_cron, $midnight_cron, $monthly_cron];
+}
+
+
+                 ####################################
+                 #              HELPERS             #
+                 ####################################
+
+sub grab_settings_for {
+    
+    my $subv_name = shift;
+    
+    my $hourly_take   = $YABSMRC_HASH{"${subv_name}_hourly_take"};
+    my $hourly_keep   = $YABSMRC_HASH{"${subv_name}_hourly_keep"};
+    my $daily_take    = $YABSMRC_HASH{"${subv_name}_daily_take"};
+    my $daily_keep    = $YABSMRC_HASH{"${subv_name}_daily_keep"};
+    my $midnight_want = $YABSMRC_HASH{"${subv_name}_midnight_want"};
+    my $midnight_keep = $YABSMRC_HASH{"${subv_name}_midnight_keep"};
+    my $monthly_want  = $YABSMRC_HASH{"${subv_name}_monthly_want"};
+    my $monthly_keep  = $YABSMRC_HASH{"${subv_name}_monthly_keep"};
+    
+    return ($hourly_take, $hourly_keep,
+            $daily_take, $daily_keep,
+            $midnight_want, $midnight_keep,
+            $monthly_want, $monthly_keep);
+}
+
+say "success";

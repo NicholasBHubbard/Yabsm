ea78e7c9f189e7e60a83260df977e89b36d70772
Nicholas Hubbard <nhub73@keemail.me>
2021-11-09 10:36:22 -0500

    install cpan modules into local::lib with plx --cpanm

---
+++ tools/cpanm.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# Install module from cpan to this projects local lib using plx
+
+YABSM_ROOT=$(realpath "$(dirname "$(readlink -f "$0")")"/..)
+
+# work from project root
+cd "$YABSM_ROOT" || (echo "error: could not cd to dir '$YABSM_ROOT'" >&2 && exit 1)
+
+if ! [ -x "$(command -v plx)" ]; then
+    echo "error: could not find program 'plx'" >&2
+    exit 1
+fi
+
+for module in "$@"
+do
+    plx --cpanm -Llocal "$module"
+done

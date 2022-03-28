6742377f3c813857fa5bbc559dc912f7054c92ab
Nicholas Hubbard <nicholashubbard@posteo.net>
2022-03-28 12:51:06 -0400

    Update to version 2.2.4

--- src/yabsm.pl
+++ src/yabsm.pl
@@ -7,7 +7,7 @@
 #  This is the toplevel script of yabsm. The actual program that is
 #  installed on the end users system is this script but fatpacked.
 
-our $VERSION = '2.2.3';
+our $VERSION = '2.2.4';
 
 use strict;
 use warnings;

4405c750f1416694cc3926778c7e21f66cfada3d
Nicholas Hubbard <nicholashubbard@posteo.net>
2022-05-31 13:29:33 -0400

    Fixed mispelling of 'detach'.

--- src/yabsmd.pl
+++ src/yabsmd.pl
@@ -82,7 +82,7 @@ sub main {
     Yabsm::Base::schedule_snapshots($config_ref, $cron_scheduler);
     Yabsm::Base::schedule_backups($config_ref, $cron_scheduler);
     
-    $cron_scheduler->run(detatch => 1, pid_file => $yabsmd_pid_file);
+    $cron_scheduler->run(detach => 1, pid_file => $yabsmd_pid_file);
 
     exit 0;
 }

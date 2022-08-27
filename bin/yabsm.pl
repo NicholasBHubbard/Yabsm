a9ab46dda65c80939a54f8804f93928eecc3c13a
Nicholas Hubbard <nicholashubbard@posteo.net>
2022-08-27 15:17:21 -0400

    Upcoming version will be 3.11

--- bin/yabsm.pl
+++ bin/yabsm.pl
@@ -7,7 +7,7 @@
 #  This is the toplevel script of yabsm. The actual program that is
 #  installed on the end users system is this script but fatpacked.
 
-our $VERSION = '3.1.0';
+our $VERSION = '3.11';
 
 use strict;
 use warnings;

8b69f8c63804bdbd673abd8d89f180441098b3a2
Nicholas Hubbard <nicholashubbard@posteo.net>
2022-08-27 14:56:04 -0400

    Overhaul logic

--- lib/Yabsm/Command/Daemon.pm
+++ lib/Yabsm/Command/Daemon.pm
@@ -12,16 +12,16 @@ package Yabsm::Command::Daemon;
 
 use Yabsm::Tools qw( :ALL );
 use Yabsm::Config::Query qw( :ALL );
+use Yabsm::Config::Parser 'parse_config_or_die';
 use Yabsm::Snap;
 use Yabsm::Backup::SSH;
 use Yabsm::Backup::Local;
-use Yabsm::Config::Parser 'parse_config_or_die';
 
 use Schedule::Cron;
 use Log::Log4perl;
+use POSIX ();
 
 use Carp 'confess';
-use POSIX;
 
                  ####################################
                  #               MAIN               #
@@ -55,75 +55,36 @@ sub yabsmd_start {
 
     arg_count_or_die(0, 0, @_);
 
-    die "yabsm: error: permission denied\n" unless i_am_root();
-    
+    die q(yabsm: error: permission denied)."\n" unless i_am_root();
+ 
     # There can only ever be one running instance of yabsmd.
     if (my $yabsmd_pid = yabsmd_pid()) {
         die "yabsm: error: yabsmd is already running as pid $yabsmd_pid\n";
     }
 
     have_prerequisites_or_die();
-
-    Log::Log4perl::init(do {
-        my $log_config = q(
-log4perl.category.Yabsm.Base       = ALL, Logfile
-log4perl.appender.Logfile          = Log::Log4perl::Appender::File
-log4perl.appender.Logfile.filename = /var/log/yabsmd.log
-log4perl.appender.Logfile.mode     = append
-log4perl.appender.Logfile.layout   = Log::Log4perl::Layout::PatternLayout
-log4perl.appender.Logfile.layout.ConversionPattern = %d [%M]: %m{chomp}%n
-);
-        \$log_config;
-    });
-
-    # Restart the daemon on a SIGHUP.
-    $SIG{HUP}    = \&yabsmd_restart;
-
-    # Gracefully exit on any signal that has a default action of terminate or
-    # dump.
-    $SIG{ABRT}   = \&cleanup_and_exit;
-    $SIG{ALRM}   = \&cleanup_and_exit;
-    $SIG{BUS}    = \&cleanup_and_exit;
-    $SIG{FPE}    = \&cleanup_and_exit;
-    $SIG{ILL}    = \&cleanup_and_exit;
-    $SIG{INT}    = \&cleanup_and_exit;
-    $SIG{IO}     = \&cleanup_and_exit;
-    $SIG{KILL}   = \&cleanup_and_exit;
-    $SIG{PIPE}   = \&cleanup_and_exit;
-    $SIG{PROF}   = \&cleanup_and_exit;
-    $SIG{PWR}    = \&cleanup_and_exit;
-    $SIG{QUIT}   = \&cleanup_and_exit;
-    $SIG{SEGV}   = \&cleanup_and_exit;
-    $SIG{STKFLT} = \&cleanup_and_exit;
-    $SIG{SYS}    = \&cleanup_and_exit;
-    $SIG{TERM}   = \&cleanup_and_exit;
-    $SIG{TRAP}   = \&cleanup_and_exit;
-    $SIG{USR1}   = \&cleanup_and_exit;
-    $SIG{USR2}   = \&cleanup_and_exit;
-    $SIG{VTALRM} = \&cleanup_and_exit;
-    $SIG{XCPU}   = \&cleanup_and_exit;
-    $SIG{XFSZ}   = \&cleanup_and_exit;
-
+    my ($yabsm_uid, $yabsm_gid) = create_yabsm_user_and_group();
     my $config_ref = parse_config_or_die();
+    install_signal_handlers();
+    init_log4perl();
+    create_runtime_dirs($config_ref);
 
-    initialize_runtime_environment($config_ref);
-
-    my $yabsm_uid = getpwnam('yabsm');
-    my $yabsm_gid = getgrnam('yabsm');
-
-    open my $fh, '>', '/run/yabsmd.pid'
-      or die q(yabsm: internal error: could not create file '/run/yabsmd.pid');
-    close $fh;
+    # Make sure pid file is owned by yabsm.
+    open my $pid_fh, '>', '/run/yabsmd.pid'
+      or die q(yabsm: internal error: cannot not open file '/run/yabsmd.pid' for writing)."\n";
+    close $pid_fh;
     chown $yabsm_uid, $yabsm_gid, '/run/yabsmd.pid';
     chmod 0644, '/run/yabsmd.pid';
 
     POSIX::setuid($yabsm_uid);
     POSIX::setgid($yabsm_gid);
 
+    create_yabsm_user_ssh_key(0, $config_ref);
+
     my $pid = create_cron_scheduler($config_ref)->run(detach => 1, pid_file => '/run/yabsmd.pid');
     
-    say $pid;
-
+    say "started yabsmd as pid $pid";
+    
     return $pid;
 }
 
@@ -133,14 +94,16 @@ sub yabsmd_stop {
 
     arg_count_or_die(0, 0, @_);
 
-    die "yabsm: error: permission denied\n" unless i_am_root();
+    die q(yabsm: error: permission denied)."\n" unless i_am_root();
     
     if (my $pid = yabsmd_pid()) {
-        say "Stopping yabsmd process running as pid $pid";
-        unless (kill 'TERM', $pid) {
-            die "yabsm: error: couldn't kill yabsmd process running as pid $pid";
+        if (kill 'TERM', $pid) {
+            say "terminated yabsmd process running as pid $pid";
+            return $pid;
         }
-        return 1;
+        else {
+            die "yabsm: error: cannot terminate yabsmd process running as pid $pid\n";
+        }
     }
     else { die "no running instance of yabsmd\n" }
 }
@@ -151,7 +114,7 @@ sub yabsmd_restart {
 
     arg_count_or_die(0, 0, @_);
 
-    die "yabsm: error: permission denied\n" unless i_am_root();
+    die q(yabsm: error: permission denied)."\n" unless i_am_root();
 
     yabsmd_stop();
     
@@ -189,7 +152,7 @@ sub create_cron_scheduler {
     my $config_ref = shift;
 
     my $cron_scheduler = Schedule::Cron->new(
-        sub { confess("yabsm: internal error: default Schedule::Cron dispatcher invoked") }
+        sub { confess("yabsm: internal error: default Schedule::Cron dispatcher was invoked") }
         , processprefix => 'yabsmd'
     );
 
@@ -328,12 +291,14 @@ sub create_cron_scheduler {
     return $cron_scheduler;
 }
 
-sub initialize_runtime_environment {
+sub create_runtime_dirs {
 
-    # Create all the directories needed for every snap, ssh_backup, and
-    # local_backup. Create a locked user named 'yabsm', and add a sudoer rule to
-    # grant this user root access to btrfs. Finally switch the processes uid and
-    # gid to the new yabsm user.
+    # Create the directories needed for every snap, ssh_backup, and local_backup.
+    # Create a locked user named 'yabsm', and a group named yabsm if they do not
+    # already exist. Add a sudoer rule to grant the yabsm user root access to
+    # btrfs.
+    #
+    # G
 
     arg_count_or_die(1, 1, @_);
 
@@ -361,14 +326,96 @@ sub initialize_runtime_environment {
             make_path_or_die(local_backup_dir($local_backup, $tframe, $config_ref));
         }
     }
-    
-    create_yabsm_user($config_ref);
-    add_btrfs_sudoer_rule();
-    
+
     return 1;
 }
 
-sub yabsm_user_home {
+sub create_yabsm_user_and_group { # Not tested
+
+    # Create a locked-user and group named 'yabsm' if they do not already exist.
+
+    arg_count_or_die(1, 1, @_);
+
+    my $config_ref = shift;
+
+    i_am_root_or_die();
+
+    unless (yabsm_user_exists()) {
+        # useradd will automatically create a group named 'yabsm'.
+        system_or_die('useradd', '-m', yabsm_user_home($config_ref), 'yabsm');
+        system_or_die('passwd', '--lock', 'yabsm');
+    }
+
+    unless (yabsm_group_exists()) {
+        system_or_die('groupadd', 'yabsm');
+    }
+
+    my $yabsm_uid = getpwnam('yabsm');
+    my $yabsm_gid = getgrnam('yabsm');
+
+    return wantarray ? ($yabsm_uid, $yabsm_gid) : 1;
+}
+
+sub yabsm_user_exists { # Not tested
+
+    # Return 1 if there exists a locked user on the system named 'yabsm'.
+
+    arg_count_or_die(0, 0, @_);
+
+    i_am_root_or_die();
+
+    unless (0 == system('getent passwd yabsm >/dev/null 2>&1')) {
+        return 0;
+    }
+
+    unless ('L' eq (split ' ', `passwd -S yabsm`)[1]) {
+        die q(yabsm: error: found non-locked user named 'yabsm')."\n";
+    }
+
+    return 1;
+}
+
+sub yabsm_group_exists { # Not tested
+
+    # Return 1 if there exists on the system a user and group named 'yabsm' and
+    # return 0 otherwise.
+
+    arg_count_or_die(0, 0, @_);
+
+    i_am_root_or_die();
+
+    return 0+(0 == system('getent group yabsm >/dev/null 2>&1'));
+}
+
+sub add_yabsm_user_btrfs_sudoer_rule { # Not tested
+
+    # Add sudoer rule to '/etc/sudoers.d/yabsm-btrfs' to grant the 'yabsm' user
+    # sudo access to btrfs-progs.
+
+    arg_count_or_die(0, 0, @_);
+
+    i_am_root_or_die();
+
+    my $file = '/etc/sudoers.d/yabsm-btrfs';
+
+    unless (-f $file) {
+        my $btrfs_bin = `which btrfs 2>/dev/null`
+          or confess('yabsm: internal error: btrfs-progs not in root users path');
+
+        my $sudoer_rule = "yabsm ALL=(root) NOPASSWD $btrfs_bin";
+
+        open my $fh, '>', $file
+          or confess("yabsm: internal error: could not open '$file' for writing");
+
+        print $fh $sudoer_rule;
+
+        close $fh
+    }
+
+    return $file;
+}
+
+sub yabsm_user_home { # Not tested
 
     # Return the yabsm users home directory.
 
@@ -377,66 +424,7 @@ sub yabsm_user_home {
     return yabsm_dir( shift ) . '/.yabsm-var/yabsm-user-home';
 }
 
-sub create_yabsm_user {
-
-    # Create a locked user on the host system named 'yabsm' or die.
-    # If there is already a locked user named 'yabsm' then just
-    # peacefully return 1.
-
-    arg_count_or_die(1, 1, @_);
-
-    my $config_ref = shift;
-
-    i_am_root_or_die();
-
-    # if yabsm user already exists
-    if (0 == system('id yabsm >/dev/null 2>&1')) {
-
-        # make sure the yabsm user is locked
-        unless ('L' eq (split ' ', `passwd -S yabsm`)[1]) {
-            confess(q(yabsm: error: found non-locked user named 'yabsm'));
-        }
-        
-        return 1;
-    }
-
-    system_or_die('useradd', '-m', '-d', yabsm_user_home($config_ref), '-k', '/dev/null', 'yabsm');
-    system_or_die('passwd', '-l', 'yabsm');
-    
-    return 1;
-}
-
-sub add_btrfs_sudoer_rule {
-
-    # Add sudoer rule to grant $user sudo access to btrfs-progs.
-
-    arg_count_or_die(0, 1, @_);
-
-    my $user = shift // 'yabsm';
-
-    i_am_root_or_die();
-
-    my $sudoer_rule_file = "/etc/sudoers.d/$user-btrfs";
-
-    unless (-f $sudoer_rule_file) {
-
-        my $btrfs_bin = `which btrfs 2>/dev/null`
-          or confess('yabsm: internal error: btrfs command not in root users path');
-
-        my $sudoer_rule = "$user ALL=(root) NOPASSWD: $btrfs_bin\n";
-
-        open my $fh, '>', $sudoer_rule_file
-          or confess("yabsm: internal error: root user could not open '$sudoer_rule_file' for writing");
-
-        print $fh $sudoer_rule;
-
-        close $fh;
-    }
-
-    return $sudoer_rule_file;
-}
-
-sub yabsmd_pid {
+sub yabsmd_pid { # Not tested
 
     # If there is a running instance of yabsmd return its pid and otherwise
     # return 0.
@@ -453,17 +441,133 @@ sub yabsmd_pid {
 
     my $is_running = $pid_file_pid && $pgrep_pid && $pid_file_pid eq $pgrep_pid;
 
-    return $is_running ? $pgrep_pid : 0;
+    return $is_running ? $pid_file_pid : 0;
 }
 
-sub cleanup_and_exit {
+sub init_log4perl { # Not tested
 
-    # Used as signal handler for all default terminating signals.
+    # TODO
 
-    if (my $yabsmd_pid = yabsmd_pid()) {
-        unlink '/run/yabsmd.pid';
-        kill 'KILL', $yabsmd_pid;
+    arg_count_or_die(0, 0, @_);
+
+    i_am_root_or_die();
+
+    my $log_file = '/var/log/yabsmd.log';
+    
+    unless (yabsm_user_exists()) {
+        die q(yabsm: internal error: cannot find user named 'yabsm')."\n";
     }
+    unless (yabsm_group_exists()) {
+        die q(yabsm: internal error: cannot find group named 'yabsm')."\n";
+    }
+
+    open my $log_fh, '>', $log_file
+      or die "yabsm: internal error: cannot open '$log_file' for writing\n";
+    close $log_fh;
+
+    my $yabsm_uid = getpwnam('yabsm');
+    my $yabsm_gid = getgrnam('yabsm');
+    chown $yabsm_uid, $yabsm_gid, $log_file;
+    chmod 0422, $log_file;
+
+    Log::Log4perl::init(do {
+        my $log_config = qq(
+log4perl.category.Yabsm.Base       = ALL, Logfile
+log4perl.appender.Logfile          = Log::Log4perl::Appender::File
+log4perl.appender.Logfile.filename = $log_file
+log4perl.appender.Logfile.mode     = append
+log4perl.appender.Logfile.layout   = Log::Log4perl::Layout::PatternLayout
+log4perl.appender.Logfile.layout.ConversionPattern = %d [%M]: %m{chomp}%n
+);
+        \$log_config;
+    });
+
+    return 1;
+}
+
+sub install_signal_handlers { # Not tested
+
+    # Handle all signals with a default action of terminate or dump so we
+    # remove /run/yabsmd.pid before terminating.
+    #
+    # Handle SIGHUP to restart yabsmd.
+
+    # Restart the daemon on a SIGHUP.
+    $SIG{HUP}    = \&yabsmd_restart;
+
+    # Gracefully exit on any signal that has a default action of terminate or
+    # dump.
+    my $cleanup_and_exit = sub {
+        unlink '/run/yabsmd.pid';
+        exit 0;
+    };
+
+    $SIG{ABRT}   = $cleanup_and_exit;
+    $SIG{ALRM}   = $cleanup_and_exit;
+    $SIG{BUS}    = $cleanup_and_exit;
+    $SIG{FPE}    = $cleanup_and_exit;
+    $SIG{ILL}    = $cleanup_and_exit;
+    $SIG{INT}    = $cleanup_and_exit;
+    $SIG{IO}     = $cleanup_and_exit;
+    $SIG{KILL}   = $cleanup_and_exit;
+    $SIG{PIPE}   = $cleanup_and_exit;
+    $SIG{PROF}   = $cleanup_and_exit;
+    $SIG{PWR}    = $cleanup_and_exit;
+    $SIG{QUIT}   = $cleanup_and_exit;
+    $SIG{SEGV}   = $cleanup_and_exit;
+    $SIG{STKFLT} = $cleanup_and_exit;
+    $SIG{SYS}    = $cleanup_and_exit;
+    $SIG{TERM}   = $cleanup_and_exit;
+    $SIG{TRAP}   = $cleanup_and_exit;
+    $SIG{USR1}   = $cleanup_and_exit;
+    $SIG{USR2}   = $cleanup_and_exit;
+    $SIG{VTALRM} = $cleanup_and_exit;
+    $SIG{XCPU}   = $cleanup_and_exit;
+    $SIG{XFSZ}   = $cleanup_and_exit;
+}
+
+sub create_yabsm_user_ssh_key { # Not tested
+
+    # Create an SSH key for the yabsm user if one doesn't already exist. This
+    # function dies unless the processes ruid and rgid are that of the yabsm user
+    # and group.
+    #
+    # If the $force value is false then only create the key if the users
+    # configuration defines at least one ssh_backup, and if it is true then
+    # create the key even if no ssh_backup's are defined.
+
+    arg_count_or_die(2, 2, @_);
+    
+    my $force      = shift;
+    my $config_ref = shift;
+
+    if ($force || all_ssh_backups($config_ref)) {
+
+        my $yabsm_uid = getpwnam('yabsm') or confess(q(yabsm: internal error: cannot find user named 'yabsm'));
+        my $yabsm_gid = getgrnam('yabsm') or confess(q(yabsm: internal error: cannot find group named 'yabsm'));
+
+        unless ($< == $yabsm_uid && $( == $yabsm_gid) {
+            my $username = getpwuid $<;
+            die "yabsm: error: cannot create yabsm users SSH key when running as user '$username'\n";
+        }
+
+        my $yabsm_user_home = yabsm_user_home($config_ref);
+
+        my $ssh_dir  = "$yabsm_user_home/.ssh";
+        my $priv_key = "$ssh_dir/id_ed25519";
+        my $pub_key  = "$ssh_dir/id_ed25519.pub";
+
+        unless (-f $priv_key && -f $pub_key) {
+            system_or_die('ssh-keygen', '-t', 'ed25519');
+            chown $yabsm_uid, $yabsm_gid, $priv_key, $pub_key;
+            chmod 0600, $priv_key;
+            chmod 0644, $pub_key;
+        }
+
+        return 1;
+    }
+
+    return 0;
 }
 
 1;

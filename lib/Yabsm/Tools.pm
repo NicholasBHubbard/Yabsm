9b73a2f0c7840a96d04ea47d1981906e23067579
Nicholas Hubbard <nicholashubbard@posteo.net>
2022-08-27 16:17:28 -0400

    Minor changes

--- lib/Yabsm/Tools.pm
+++ lib/Yabsm/Tools.pm
@@ -113,7 +113,8 @@ sub with_error_catch_log { # Is tested
         $sub->(@args);
     }
     catch ($e) {
-        Log::Log4Perl->get_logger->error($e);
+        my $logger = Log::Log4perl::get_logger;
+        $logger->error($e);
     }
 }
 

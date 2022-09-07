5fbeec8243dc8afa8c13e02ce2b7198f614d0f3b
Nicholas Hubbard <nicholashubbard@posteo.net>
2022-09-06 23:34:14 -0400

    Added some useful commands

--- lib/Yabsm/Command/Config.pm
+++ lib/Yabsm/Command/Config.pm
@@ -18,20 +18,25 @@ use Yabsm::Config::Parser 'parse_config_or_die';
                  #               MAIN               #
                  ####################################
 
-my $usage = 'usage: yabsm config <check ?file|subvols|snaps|ssh_backups|local_backups|backups>'."\n";
+sub die_usage {
+    arg_count_or_die(0, 0, @_);
+    die 'usage: yabsm config <check ?file|subvols|snaps|ssh_backups|local_backups|backups|yabsm_dir|yabsm_user_home>'."\n";
+}
 
 sub main {
 
-    my $cmd = shift or die $usage;
+    my $cmd = shift or die_usage();
 
-    if    ($cmd eq 'subvols'       ) { print_subvols(@_)       }
-    elsif ($cmd eq 'snaps'         ) { print_snaps(@_)         }
-    elsif ($cmd eq 'ssh_backups'   ) { print_ssh_backups(@_)   }
-    elsif ($cmd eq 'local_backups' ) { print_local_backups(@_) }
-    elsif ($cmd eq 'backups'       ) { print_backups(@_)       }
-    elsif ($cmd eq 'check'         ) { check_config(@_)        }
+    if    ($cmd eq 'subvols'         ) { print_subvols(@_)         }
+    elsif ($cmd eq 'snaps'           ) { print_snaps(@_)           }
+    elsif ($cmd eq 'ssh_backups'     ) { print_ssh_backups(@_)     }
+    elsif ($cmd eq 'local_backups'   ) { print_local_backups(@_)   }
+    elsif ($cmd eq 'backups'         ) { print_backups(@_)         }
+    elsif ($cmd eq 'yabsm_user_home' ) { print_yabsm_user_home(@_) }
+    elsif ($cmd eq 'check'           ) { check_config(@_)          }
+    elsif ($cmd eq 'yabsm_dir'       ) { print_yabsm_dir(@_)       }
     else {
-        die $usage;
+        die_usage();
     }
 
     exit 0;
@@ -42,31 +47,31 @@ sub main {
                  ####################################
 
 sub print_subvols {
-    0 == @_ or die $usage;
+    0 == @_ or die_usage();
     my $config_ref = parse_config_or_die();
     say for all_subvols($config_ref);
 }
 
 sub print_snaps {
-    0 == @_ or die $usage;
+    0 == @_ or die_usage();
     my $config_ref = parse_config_or_die();
     say for all_snaps($config_ref);
 }
 
 sub print_ssh_backups {
-    0 == @_ or die $usage;
+    0 == @_ or die_usage();
     my $config_ref = parse_config_or_die();
     say for all_ssh_backups($config_ref);
 }
 
 sub print_local_backups {
-    0 == @_ or die $usage;
+    0 == @_ or die_usage();
     my $config_ref = parse_config_or_die();
     say for all_local_backups($config_ref);
 }
 
 sub print_backups {
-    0 == @_ or die $usage;
+    0 == @_ or die_usage();
     my $config_ref = parse_config_or_die();
     my @ssh_backups = all_ssh_backups($config_ref);
     my @local_backups = all_local_backups($config_ref);
@@ -74,10 +79,24 @@ sub print_backups {
 }
 
 sub check_config {
-    1 >= @_ or die $usage;
+    1 >= @_ or die_usage();
     my $file = shift // '/etc/yabsm.conf';
     parse_config_or_die($file);
     say 'all good';
+}
+
+sub print_yabsm_dir {
+    0 == @_ or die_usage();
+    my $config_ref = parse_config_or_die();
+    my $yabsm_dir = yabsm_dir($config_ref);
+    say $yabsm_dir;
+}
+
+sub print_yabsm_user_home {
+    0 == @_ or die_usage();
+    my $config_ref = parse_config_or_die();
+    my $yabsm_user_home = yabsm_user_home($config_ref);
+    say $yabsm_user_home;
 }
 
 1;

c812d9e4bb908188a08ed2a7aecb96934b424c7f
Nicholas Hubbard <nicholashubbard@posteo.net>
2022-09-15 17:59:00 -0400

    Redirect stderr

--- lib/Yabsm/Backup/SSH.pm
+++ lib/Yabsm/Backup/SSH.pm
@@ -274,19 +274,19 @@ add_error() {
 
 HAVE_BTRFS=true
 
-if ! which btrfs; then
+if ! which btrfs >/dev/null 2>&1; then
    HAVE_BTRFS=false
    add_error "btrfs-progs not in '\$(whoami)'s path"
 fi
 
-if [ "\$HAVE_BTRFS" = true ] && ! sudo -n btrfs --help; then
+if [ "\$HAVE_BTRFS" = true ] && ! sudo -n btrfs --help >/dev/null 2>&1; then
     add_error "user '\$(whoami)' does not have root sudo access to btrfs-progs"
 fi
 
 if ! [ -d '$backup_dir' ] || ! [ -r '$backup_dir' ] || ! [ -w '$backup_dir' ]; then
     add_error "no directory named '$backup_dir' that is readable+writable to user '\$(whoami)'"
 else
-    if [ "\$HAVE_BTRFS" = true ] && ! btrfs property list '$backup_dir'; then
+    if [ "\$HAVE_BTRFS" = true ] && ! btrfs property list '$backup_dir' >/dev/null 2>&1; then
         add_error "'$backup_dir' is not a directory residing on a btrfs filesystem"
     fi
 fi

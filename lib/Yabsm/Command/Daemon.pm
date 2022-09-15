aac7d4fee552468f3fb75e0bf87bca8974777d65
Nicholas Hubbard <nicholashubbard@posteo.net>
2022-09-15 00:41:57 -0400

    Documentation

--- lib/Yabsm/Command/Daemon.pm
+++ lib/Yabsm/Command/Daemon.pm
@@ -557,7 +557,6 @@ sub create_yabsm_user_and_group {
     i_am_root_or_die();
 
     unless (yabsm_user_exists()) {
-        # useradd will automatically create a group named 'yabsm'.
         system_or_die('useradd', '-m', '-d', yabsm_user_home($config_ref), '-s', '/bin/sh', '-k', '/dev/null', 'yabsm');
         system_or_die('passwd', '--lock', 'yabsm');
     }

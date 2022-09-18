4f6147f2c3da83843297b2111a43e52f3bcf93af
Nicholas Hubbard <nicholashubbard@posteo.net>
2022-09-17 20:05:52 -0400

    Changed language around bootstrap snapshots

--- lib/Yabsm/Backup/Generic.pm
+++ lib/Yabsm/Backup/Generic.pm
@@ -41,11 +41,10 @@ our @EXPORT_OK = qw(take_bootstrap_snapshot
                  #            SUBROUTINES           #
                  ####################################
 
-sub take_bootstrap_snapshot {
+sub take_tmp_snapshot {
 
-    # Take a btrfs bootstrap snapshot of $backup and return its path.
-    # If there is already a bootstrap snapshot for $backup then delete
-    # it and take a new one.
+    # Take a tmp snapshot for $backup. A tmp snapshot is necessary for taking an
+    # incremental backup with 'btrfs send -p'.
 
     arg_count_or_die(3, 3, @_);
 
@@ -53,160 +52,28 @@ sub take_bootstrap_snapshot {
     my $backup_type = shift;
     my $config_ref  = shift;
 
+    my $tmp_snapshot_dir = tmp_snapshot_dir($backup, $backup_type, $config_ref);
+
+    # Remove any old tmp snapshots
+    opendir my $dh, $tmp_snapshot_dir or confess("yabsm: internal error: cannot opendir '$tmp_snapshot_dir'");
+    my @tmp_snapshots = grep { is_snapshot_name($_, ALLOW_BOOTSTRAP => 0) } readdir($dh);
+    map { $_ = "$tmp_snapshot_dir/$_" } @tmp_snapshots;
+    closedir $dh;
+    delete_snapshot($_) for @tmp_snapshots;
+    
     my $mountpoint;
 
-    if ($backup_type eq 'ssh') {
+    if ($backup_type eq 'ssh')   {
         $mountpoint = ssh_backup_mountpoint($backup, $config_ref);
     }
     elsif ($backup_type eq 'local') {
         $mountpoint = local_backup_mountpoint($backup, $config_ref);
     }
-    else { is_backup_type_or_die($backup_type) }
-    
-    if (my $bootstrap_snapshot = backup_bootstrap_snapshot($backup, $backup_type, $config_ref)) {
-        delete_snapshot($bootstrap_snapshot);
-    }
-    
-    my $bootstrap_dir = bootstrap_snapshot_dir($backup, $backup_type, $config_ref);
-
-    my $snapshot_name = '.BOOTSTRAP-' . current_time_snapshot_name();
-    
-    return take_snapshot($mountpoint, $bootstrap_dir, $snapshot_name);
-}
-
-sub maybe_take_bootstrap_snapshot {
-
-    # If $backup does not already have a bootstrap snapshot then take
-    # a bootstrap snapshot and return its path. Otherwise return the
-    # path of the existing bootstrap snapshot.
-    
-    arg_count_or_die(3, 3, @_);
-
-    my $backup      = shift;
-    my $backup_type = shift;
-    my $config_ref  = shift;
-    
-    if (my $bbs = backup_bootstrap_snapshot($backup, $backup_type, $config_ref)) {
-        return $bbs;
-    }
-
-    return take_bootstrap_snapshot($backup, $backup_type, $config_ref);
-}
-
-sub backup_bootstrap_snapshot {
-
-    # If the backup $backup has a bootstrap snapshot return its path
-    # and otherwise return undef.
-
-    arg_count_or_die(3, 3, @_);
-
-    my $backup      = shift;
-    my $backup_type = shift;
-    my $config_ref  = shift;
-
-    is_backup_type_or_die($backup_type);
-    backup_exists_or_die($backup, $config_ref);
-
-    my $bootstrap_dir = bootstrap_snapshot_dir($backup, $backup_type, $config_ref);
-
-    is_btrfs_dir_or_die($bootstrap_dir);
-
-    opendir my $dh, $bootstrap_dir or confess("yabsm: internal error: cannot opendir '$bootstrap_dir'");
-    my @boot_snapshots = map { $_ = "$bootstrap_dir/$_" } grep { $_ !~ /^(\.\.|\.)$/ } readdir($dh);
-    closedir $dh;
-
-    if (0 == @boot_snapshots) {
-        return undef;
-    }
-    elsif (1 == @boot_snapshots) {
-        my $bootstrap_snapshot = shift @boot_snapshots;
-        is_bootstrap_snapshot_or_die($bootstrap_snapshot);
-        return $bootstrap_snapshot;
-    }
     else {
-        die "yabsm: error: found multiple files in '$bootstrap_dir': " . map { $_ = q('$_') } @boot_snapshots . "\n";
+        is_backup_type_or_die($backup_type);
     }
-}
-
-sub bootstrap_snapshot_dir {
-
-    # Return the path to $ssh_backup's bootstrap snapshot directory.
-    # Logdie if the bootstrap snapshot directory does not exist.
-
-    arg_count_or_die(3, 3, @_);
-
-    my $backup      = shift;
-    my $backup_type = shift;
-    my $config_ref  = shift;
-
-    is_backup_type_or_die($backup_type);
-
-    if ($backup_type eq 'ssh') {
-        ssh_backup_exists_or_die($backup, $config_ref);
-    }
-    if ($backup_type eq 'local') {
-        local_backup_exists_or_die($backup, $config_ref);
-    }
-
-    return yabsm_dir($config_ref) . "/.yabsm-var/${backup_type}_backups/$backup/bootstrap-snapshot";
-}
-
-sub is_bootstrap_snapshot {
-
-    # Return 1 if $bootstrap_snapshot is a btrfs subvolume that has a
-    # basename that is a valid yabsm bootstrap snapshot name.
-
-    arg_count_or_die(1, 1, @_);
-
-    my $bootstrap_snapshot = shift;
-
-    return 0 unless is_btrfs_subvolume($bootstrap_snapshot);
-    return 0 unless is_bootstrap_snapshot_name(basename($bootstrap_snapshot));
-
-    return 1;
-}
-
-sub is_bootstrap_snapshot_or_die {
-
-    # Wrapper around &is_bootstrap_snapshot that logdies if it returns
-    # false.
-
-    arg_count_or_die(1, 1, @_);
-
-    my $snapshot = shift;
-
-    is_bootstrap_snapshot_name_or_die(basename($snapshot));
-    is_btrfs_subvolume_or_die($snapshot);
-
-    return 1;
-}
-
-sub is_bootstrap_snapshot_name {
-
-    # Return 1 if given a valid yabsm bootstrap snapshot name and
-    # return 0 otherwise.
-    #
-    # Note that this function does not accept file paths.
-
-    arg_count_or_die(1, 1, @_);
-
-    return 0+(shift =~ /^\.BOOTSTRAP-yabsm-\d{4}_\d{2}_\d{2}_\d{2}:\d{2}$/);
-}
-
-sub is_bootstrap_snapshot_name_or_die {
-
-    # Wrapper around is_bootstrap_snapshot_name that logdies if it
-    # returns false.
-
-    arg_count_or_die(1, 1, @_);
-
-    my $snapshot_name = shift;
-
-    unless ( is_bootstrap_snapshot_name($snapshot_name) ) {
-        confess("yabsm: internal error: '$snapshot_name' is not a valid yabsm bootstrap snapshot name");
-    }
-
-    return 1;
+    
+    return take_snapshot($mountpoint, $tmp_snapshot_dir);
 }
 
 sub tmp_snapshot_dir {
@@ -232,10 +99,11 @@ sub tmp_snapshot_dir {
     return yabsm_dir($config_ref) . "/.yabsm-var/${backup_type}_backups/$backup/tmp-snapshot";
 }
 
-sub take_tmp_snapshot {
+sub take_bootstrap_snapshot {
 
-    # Take a tmp snapshot for $backup. A tmp snapshot is necessary for taking an
-    # incremental backup with 'btrfs send -p'.
+    # Take a btrfs bootstrap snapshot of $backup and return its path.
+    # If there is already a bootstrap snapshot for $backup then delete
+    # it and take a new one.
 
     arg_count_or_die(3, 3, @_);
 
@@ -243,33 +111,72 @@ sub take_tmp_snapshot {
     my $backup_type = shift;
     my $config_ref  = shift;
 
-    my $tmp_snapshot_dir = tmp_snapshot_dir($backup, $backup_type, $config_ref);
-
-    # Remove any old tmp snapshots
-    opendir my $dh, $tmp_snapshot_dir or confess("yabsm: internal error: cannot opendir '$tmp_snapshot_dir'");
-    my @tmp_snapshots = map { $_ = "$tmp_snapshot_dir/$_" } grep { is_snapshot_name($_, 0) } readdir($dh);
-    closedir $dh;
-    delete_snapshot($_) for @tmp_snapshots;
-    
     my $mountpoint;
 
-    if ($backup_type eq 'ssh')   {
+    if ($backup_type eq 'ssh') {
         $mountpoint = ssh_backup_mountpoint($backup, $config_ref);
     }
     elsif ($backup_type eq 'local') {
         $mountpoint = local_backup_mountpoint($backup, $config_ref);
     }
-    else {
-        is_backup_type_or_die($backup_type);
+    else { is_backup_type_or_die($backup_type) }
+    
+    if (my $bootstrap_snapshot = the_local_bootstrap_snapshot($backup, $backup_type, $config_ref)) {
+        delete_snapshot($bootstrap_snapshot);
     }
     
-    return take_snapshot($mountpoint, $tmp_snapshot_dir);
+    my $bootstrap_dir = bootstrap_snapshot_dir($backup, $backup_type, $config_ref);
+    my $snapshot_name = '.BOOTSTRAP-' . current_time_snapshot_name();
+    
+    return take_snapshot($mountpoint, $bootstrap_dir, $snapshot_name);
 }
 
-sub the_bootstrap_snapshot {
+sub maybe_take_bootstrap_snapshot {
 
-    # Return the bootstrap snapshot for $backup if it exists and return 0
-    # otherwise. Die if there are multiple bootstrap snapshots.
+    # If $backup does not already have a bootstrap snapshot then take
+    # a bootstrap snapshot and return its path. Otherwise return the
+    # path of the existing bootstrap snapshot.
+    
+    arg_count_or_die(3, 3, @_);
+
+    my $backup      = shift;
+    my $backup_type = shift;
+    my $config_ref  = shift;
+    
+    if (my $boot_snap = the_local_bootstrap_snapshot($backup, $backup_type, $config_ref)) {
+        return $boot_snap;
+    }
+
+    return take_bootstrap_snapshot($backup, $backup_type, $config_ref);
+}
+
+sub bootstrap_snapshot_dir {
+
+    # Return the path to $ssh_backup's bootstrap snapshot directory.
+    # Logdie if the bootstrap snapshot directory does not exist.
+
+    arg_count_or_die(3, 3, @_);
+
+    my $backup      = shift;
+    my $backup_type = shift;
+    my $config_ref  = shift;
+
+    is_backup_type_or_die($backup_type);
+
+    if ($backup_type eq 'ssh') {
+        ssh_backup_exists_or_die($backup, $config_ref);
+    }
+    if ($backup_type eq 'local') {
+        local_backup_exists_or_die($backup, $config_ref);
+    }
+
+    return yabsm_dir($config_ref) . "/.yabsm-var/${backup_type}_backups/$backup/bootstrap-snapshot";
+}
+
+sub the_local_bootstrap_snapshot {
+
+    # Return the local bootstrap snapshot for $backup if it exists and return
+    # undef otherwise. Die if there are multiple bootstrap snapshots.
 
     arg_count_or_die(3, 3, @_);
 
@@ -280,8 +187,19 @@ sub the_bootstrap_snapshot {
     my $bootstrap_dir = bootstrap_snapshot_dir($backup, $backup_type, $config_ref);
 
     opendir my $dh, $bootstrap_dir or confess "yabsm: internal error: cannot opendir '$bootstrap_dir'";
-    my @boot_snaps = grep { is_snapshot_name} map { $_ = "$bootstrap_dir/$_" } readdir($dh);
+    my @boot_snaps = grep { is_snapshot_name($_, ONLY_BOOTSTRAP => 1) } readdir($dh);
+    map { $_ = "$bootstrap_dir/$_" } @boot_snaps;
     close $dh;
+
+    if (1 == @boot_snaps) {
+        return $boot_snaps[0];
+    }
+    elsif (0 == @boot_snaps) {
+        return undef;
+    }
+    else {
+        die "yabsm: error: found multiple local bootstrap snapshots for ${backup_type}_backup '$backup' in '$bootstrap_dir'\n";
+    }
 }
 
 sub bootstrap_lock_file {

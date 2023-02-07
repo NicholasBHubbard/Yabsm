7bae26d809a02304aadba8ca8d1c7ec26045d463
Nicholas Hubbard <nicholashubbard@posteo.net>
2023-02-07 14:46:34 -0500

    Documentation upgrades

--- lib/App/Yabsm.pm
+++ lib/App/Yabsm.pm
@@ -2,7 +2,7 @@
 #  WWW:     https://github.com/NicholasBHubbard/yabsm
 #  License: MIT
 
-#  The main module of Yabsm.
+#  This module contains the program's &main function.
 
 use strict;
 use warnings;
@@ -20,17 +20,18 @@ sub usage {
     return <<'END_USAGE';
 usage: yabsm [--help] [--version] [<COMMAND> <ARGS>]
 
-see 'man yabsm' for a detailed overview of yabsm.
+See 'man yabsm' for a detailed overview.
 
-commands:
+Commands:
 
-<config|c> [--help] [check ?file] [ssh-check <SSH_BACKUP>] [ssh-key]
-           [yabsm-user-home] [yabsm_dir] [subvols] [snaps] [ssh_backups]
-           [local_backups] [backups]
+  <daemon|d> [--help] [start] [stop] [restart] [status] [init]
 
-<find|f>   [--help] [<SNAP|SSH_BACKUP|LOCAL_BACKUP> <QUERY>]
+  <config|c> [--help] [check ?file] [ssh-check <SSH_BACKUP>] [ssh-key]
+             [yabsm-user-home] [yabsm_dir] [subvols] [snaps] [ssh_backups]
+             [local_backups] [backups]
 
-<daemon|d> [--help] [start] [stop] [restart] [status] [init]
+  <find|f>   [--help] [<SNAP|SSH_BACKUP|LOCAL_BACKUP> <QUERY>]
+
 END_USAGE
 }
 
@@ -45,11 +46,12 @@ sub main {
     if ($cmd =~ /^(-h|--help)$/) { print usage() and exit 0 }
     if ($cmd eq '--version')     { say $VERSION  and exit 0 }
 
-    # provide user with command abbreviations
+    # Provide user with command abbreviations
     if    ($cmd eq 'd') { $cmd = 'daemon' }
     elsif ($cmd eq 'c') { $cmd = 'config' }
     elsif ($cmd eq 'f') { $cmd = 'find'   }
 
+    # All 3 subcommands have their own &main
     if    ($cmd eq 'daemon') { $cmd = \&App::Yabsm::Command::Daemon::main }
     elsif ($cmd eq 'config') { $cmd = \&App::Yabsm::Command::Config::main }
     elsif ($cmd eq 'find'  ) { $cmd = \&App::Yabsm::Command::Find::main   }
